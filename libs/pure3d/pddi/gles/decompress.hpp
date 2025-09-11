3c2147c2ee43ec5e55c4f3716afad0087049262e
bruceleeto <astevens092a@gmail.com>
2025-09-11 12:19:58 +0100

    lil clean up

--- libs/pure3d/pddi/gles/decompress.hpp
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-#include <stdint.h>
-
-void BlockDecompressImageBC1(uint32_t width, uint32_t height, const uint8_t* blockStorage, unsigned char* image);
-void BlockDecompressImageBC2(uint32_t width, uint32_t height, const uint8_t* blockStorage, unsigned char* image);
-void BlockDecompressImageBC3(uint32_t width, uint32_t height, const uint8_t* blockStorage, unsigned char* image);
